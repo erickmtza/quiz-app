bc0c961682d6036acb7c116981e739f8d9e7e3bd
Erick Martinez <erick.martinez2013@gmail.com>
2019-01-13 22:07:47 -0600

    finalized project

---
+++ data.js
@@ -0,0 +1,112 @@
+const qData = [
+    {
+        question: 'Fifa is an acronym for:',
+        choices: [
+            'Fédération Internationale de Football Association',
+            'Fédération Infiniment de Football Actualité',
+            'Fabuleux Internationale Football Association',
+            'Fédération Internationale de Football Apprivoiser'
+        ],
+        answer: 'Fédération Internationale de Football Association',
+        img: 'class="win" src="https://media.giphy.com/media/GpLEhXX15eG5y/giphy.gif" alt="Fifa executives"'
+    },
+    {
+        question: 'Which country hosted the first world cup?',
+        choices: [
+            'England',
+            'Brazil',
+            'Uruguay',
+            'France'
+        ],
+        answer: 'Uruguay',
+        img: 'class="win" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Flag-map_of_Uruguay.svg/2000px-Flag-map_of_Uruguay.svg.png" alt="Fifa executives"'
+    },
+    {
+        question: 'Country with the most World Cup wins?',
+        choices: [
+            'Germany',
+            'Argentina',
+            'Brazil',
+            'Italy'
+        ],
+        answer: 'Brazil',
+        img: 'class="win" src="https://vignette.wikia.nocookie.net/tardis/images/3/38/Flag_of_Brazil.png/revision/latest?cb=20130224025921" alt="Brazilian flag"'
+    },
+    {
+        question: 'The World Cup is played every:',
+        choices: [
+            '2 years',
+            '36 months',
+            '4 years',
+            'year'
+        ],
+        answer: '4 years',
+        img: 'class="win" src="http://thedarjeelingchronicle.com/wp-content/uploads/2018/06/1986-World-Cup.jpg" alt="Maradona holds the world cup"'
+    },
+    {
+        question: 'World cup is the world\'s most widely viewed sporting event estimating at:',
+        choices: [
+            'almost 50% of the world',
+            'almost 35% of the world',
+            '5% of the world',
+            'almost 20% of the world'
+        ],
+        answer: 'almost 50% of the world',
+        img: 'class="win" src="https://i.imgur.com/PWRSH4E.png" alt="Chart representing amount of views for each major sport"'
+    },
+    {
+        question: 'The country with the most world cup wins holds the record at:',
+        choices: [
+            '4',
+            '6',
+            '3',
+            '5'
+        ],
+        answer: '5',
+        img: 'class="win" src="http://www.admin.playerrivalry.com/Uploads/media_content/brazil-past-world-cup-trophies-till-2014-fb871853.jpg?404=img_comming_Soon.png&w=1220" alt="Brazil and its five world cup trophies"'
+    },
+    {
+        question: 'PLayer to score the most goals in a single World Cup match?',
+        choices: [
+            'Zlatan Ibrahimović (Sweden)',
+            'Miroslav Klose (Germany)',
+            'Oleg Salenko (Russia)',
+            'Cristiano Ronaldo (Portugal)'
+        ],
+        answer: 'Oleg Salenko (Russia)',
+        img: 'class="win" src="https://www.dreamteamfc.com/c/wp-content/uploads/sites/4/2018/06/NINTCHDBPICT000096963686-e1528981336146.jpg?strip=all&w=662&quality=100" alt="Oleg Salenko"'
+    },
+    {
+        question: 'When did the first World Cup event take place?',
+        choices: [
+            '1904',
+            '1930',
+            '1938',
+            '1950'
+        ],
+        answer: '1930',
+        img: 'class="win" src="https://images.gulfnews.com/polopoly-images/2018/5/14/1.2221355_156403766.png" alt="Uruguay host of the 1930 world cup poster"'
+    },
+    {
+        question: 'The fastest goal ever scored in a Wold Cup was in:',
+        choices: [
+            'the first 11 seconds of the match',
+            'the 35th second',
+            'the 23rd second',
+            'the 47th second'
+        ],
+        answer: 'the first 11 seconds of the match',
+        img: 'class="win" src="https://i.pinimg.com/originals/d9/f5/17/d9f51701c5198d5a8961d4694d6f6605.gif" alt="Hakan Sukur from Turkey scores fastest goal in a world cup match"'
+    },
+    {
+        question: 'All World Cups have been won by:',
+        choices: [
+            'European teams',
+            'South American or European teams',
+            'South American/North American or European teams',
+            'South American or European or African teams'
+        ],
+        answer: 'South American or European teams',
+        img: 'class="win" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-oLiYDSlC5QK6xCSR8Z6k6k7nvoT-Dc-_NTum1dKYy-v6O7sq" alt="Europe and South America\'s best footballers"'
+    }
+]
---
+++ script.js
@@ -0,0 +1,150 @@
+'use strict';
+
+let questionIndex = 0;
+let score = 0;
+
+let quizLength = qData.length; //If more questions are added or removed
+let questionCount = 0;
+
+$(function numQuestions() {
+    $('.js-quiz-length').text(quizLength);
+})
+
+function startQuiz() {
+    $('.js-start-button').on('click', function() {
+        $('.initiate-quiz').css('display', 'none');
+        renderQuestion();
+    });
+}
+
+function generateQuestion() {
+    return `<fieldset class="quiz">
+        <legend>${qData[questionIndex].question}</legend>
+            <p>
+                <input type="radio" name="option" id="ans_1" value="${qData[questionIndex].choices[0]}" required>
+                <label for="ans_1">${qData[questionIndex].choices[0]}</label>
+            </p>
+            <p>
+                <input type="radio" name="option" id="ans_2" value="${qData[questionIndex].choices[1]}">
+                <label for="ans_2">${qData[questionIndex].choices[1]}</label>
+            </p>
+            <p>
+                <input type="radio" name="option" id="ans_3" value="${qData[questionIndex].choices[2]}">
+                <label for="ans_3">${qData[questionIndex].choices[2]}</label>
+            </p>
+            <p>
+                <input type="radio" name="option" id="ans_4" value="${qData[questionIndex].choices[3]}">
+                <label for="ans_4">${qData[questionIndex].choices[3]}</label>
+            </p>
+            <button class="check-answer js-check-answer">Next</button>
+    </fieldset>`;
+}
+
+function questionNumCount() {
+    $('.js-question-num').text(++questionCount);
+}
+
+function renderQuestion() {
+    $('.quiz-container').append(generateQuestion());
+    questionNumCount();
+}
+
+function removeQuizView() {
+    $('fieldset').remove();
+}
+
+function feedback(feedback) {
+    $('main').append(feedback);
+}
+
+function correctFeedback() {
+    return `<div class="ans-feedback js-ans-feedback">
+    <p>Correct</p>
+    <img ${qData[questionIndex].img}>
+    <button type=button class="next-quest js-next-quest">Continue</button>
+  </div>`
+}
+
+function wrongFeedback(){
+    return `<div class="ans-feedback js-ans-feedback">
+    <p>Actually the correct answer is ${qData[questionIndex].answer}.</p>
+    <img ${qData[questionIndex].img}>
+    <button type=button class="next-quest js-next-quest">Continue</button>
+  </div>`
+}
+
+function updateScore() {
+    ++score;
+    $('.js-score').text(score);
+}
+
+function revealAnswer() {
+    //when user clicks next, opens up feedback
+    $('.quiz-container').on('submit', function(event) {
+        event.preventDefault();
+        let userAnswer = $('input:checked').val();
+        let answer = qData[questionIndex].answer;
+        //if correct
+        if (userAnswer === answer) {
+            updateScore()
+            removeQuizView();
+            feedback(correctFeedback());
+        } else { 
+        //if wrong
+            removeQuizView();
+            feedback(wrongFeedback());
+        }
+    })
+}
+
+function advanceIndex() {
+    ++questionIndex;
+}
+
+function removeFeedback() {
+    $('.js-ans-feedback').remove();
+}
+
+function nextQuestion() {
+    //when user clicks continue to next question
+    $('main').on('click', '.js-next-quest', function(event) {
+        if (questionCount < quizLength) {
+            //increases questionIndex count by 1
+            advanceIndex();
+            renderQuestion();
+            //closes the feedback
+            removeFeedback();
+        } else {
+            removeFeedback();
+            resultsView();
+        }
+    })
+}
+
+function resultsView() {
+    $('main').append(result());
+}
+
+function result() {
+    return `<div class="results">
+    <p>Hopefully you learned something new. Many nationalities meet on the field and create history in the name of football.</p>
+    <p><span class="js-score">${score}</span> out of <span class="js-quiz-length">${quizLength}</span> correct.</p>
+    <img class="win" src="https://www.emugifs.net/wp-content/uploads/2018/07/France-vs-Croatia-4-2-FIFA-World-Cup-Russia-2018-Final-Victories.gif" alt="France celebrates world cup win">
+    <button class="play-again" type=button>Play Again</button>
+    </div>`;
+}
+
+function playAgain() {
+    $('main').on('click', '.play-again', function() {
+        location.reload();
+    })
+}
+
+function quizApp() {
+    startQuiz();
+    revealAnswer();
+    nextQuestion();
+    playAgain();
+}
+
+$(quizApp);
